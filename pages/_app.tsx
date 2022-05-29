2fa668c7e16532a54393b5ac724af9b70aaceb92
philipalexander <philip@dennisdevelopment.com>
2022-05-29 12:36:20 -0400

    feat: :sparkles: add layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Head from "next/head"
+import Layout from "layout"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,7 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
