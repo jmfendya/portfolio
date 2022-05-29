5c18f35c621b589cdeb2306a1da009ef30241947
philipalexander <philip@dennisdevelopment.com>
2022-05-29 12:24:18 -0400

    feat: :sparkles: add custom next Head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Portfolio site with work examples, experience, education, and contact information for Jesse Fendya."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
