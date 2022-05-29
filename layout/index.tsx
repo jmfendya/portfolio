c48b471f79132e05476824cdfaa81a649cede444
philipalexander <philip@dennisdevelopment.com>
2022-05-29 12:51:02 -0400

    refactor: :sparkles: add Navbar

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,4 +1,4 @@
-// import { Navbar } from "components/ui"
+import { Navbar } from "components/ui"
 import styles from "./layout.module.css"
 
 type Props = {
@@ -9,7 +9,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       {/* Navbar */}
-      {/* <Navbar /> */}
+      <Navbar />
 
       {/* childrem */}
       <div className={styles.wrapper}>{children}</div>
