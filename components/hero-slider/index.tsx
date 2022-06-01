1cba079a2a9c984c268cf8183bdf6976c236b63c
philipalexander <philip@dennisdevelopment.com>
2022-06-01 19:17:21 -0400

    feat: :sparkles: add hero-slider; first iteration

--- components/hero-slider/index.tsx
+++ components/hero-slider/index.tsx
@@ -7,24 +7,56 @@ import styles from "./hero-slider.module.css"
 
 interface SliderProps {
   children: ReactNode
+  looped: boolean
+  sliderIsReady?: () => void
 }
 
-const HeroSlider = ({ children }: SliderProps) => {
+const HeroSlider = ({ children, looped, sliderIsReady }: SliderProps) => {
   const [loaded, setLoaded] = useState(false)
   const [currentSlide, setCurrentSlide] = useState(0)
 
-  const loop = true
-
-  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
-    initial: 0,
-    loop,
-    created() {
-      setLoaded(true)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
+    {
+      initial: 0,
+      loop: looped,
+      slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel)
+      },
+      created(slider) {
+        setLoaded(true)
+      },
     },
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel)
-    },
-  })
+    [
+      (slider) => {
+        let timeout: ReturnType<typeof setTimeout>
+        let mouseOver = false
+        function clearNextTimeout() {
+          clearTimeout(timeout)
+        }
+        function nextTimeout() {
+          clearTimeout(timeout)
+          if (mouseOver) return
+          timeout = setTimeout(() => {
+            slider.next()
+          }, 2000)
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true
+            clearNextTimeout()
+          })
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false
+            nextTimeout()
+          })
+          nextTimeout()
+        })
+        slider.on("dragStarted", clearNextTimeout)
+        slider.on("animationEnded", nextTimeout)
+        slider.on("updated", nextTimeout)
+      },
+    ]
+  )
 
   return (
     <div className={styles["navigation-wrapper"]}>
@@ -42,45 +74,51 @@ const HeroSlider = ({ children }: SliderProps) => {
           })
         })}
       </div>
+      <>
+        {/* NAVIGATION ARROWS */}
+        {looped && loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
 
-      {/* NAVIGATION ARROWS */}
-      {loaded && instanceRef.current && (
-        <>
-          <Arrow
-            left
-            onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()}
-            disabled={!loop && currentSlide === 0}
-          />
-
-          <Arrow
-            onClick={(e) => e.stopPropagation() || instanceRef.current?.next()}
-            disabled={
-              !loop &&
-              currentSlide ===
+            <Arrow
+              onClick={(e) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
                 instanceRef.current.track.details.slides.length - 1
-            }
-          />
-        </>
-      )}
+              }
+            />
+          </>
+        )}
 
-      {/* NAVIGATION DOTS */}
-      {loaded && instanceRef.current && (
-        <div className={styles.dots}>
-          {[
-            ...Array(instanceRef.current.track.details.slides.length).keys(),
-          ].map((idx) => {
-            return (
-              <button
-                key={idx}
-                onClick={() => {
-                  instanceRef.current?.moveToIdx(idx)
-                }}
-                className={styles.dot + (currentSlide === idx ? " active" : "")}
-              ></button>
-            )
-          })}
-        </div>
-      )}
+        {/* NAVIGATION DOTS */}
+        {looped && loaded && instanceRef.current && (
+          <div className={styles.dots}>
+            {[
+              ...Array(instanceRef.current.track.details.slides.length).keys(),
+            ].map((idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx)
+                  }}
+                  className={
+                    styles.dot + (currentSlide === idx ? " active" : "")
+                  }
+                ></button>
+              )
+            })}
+          </div>
+        )}
+      </>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,92 @@
-import type { NextPage } from "next"
-import Head from "next/head"
+import type { InferGetStaticPropsType } from "next"
+import { fetcher } from "lib/contentful/fetchGraphQL"
+import { GET_HOME_SLIDER_COLLECTION } from "graphQL/queries"
+import HeroSlider from "components/hero-slider"
 import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import { useEffect, useState } from "react"
 
-const Home: NextPage = () => {
-  return <span>HomePage</span>
+type SliderImage = {
+  url: string
+  width: number
+  height: number
+  alt: string
+  sys: {}
+  title: string
+  fileName: string
+}
+
+export const getStaticProps = async () => {
+  const { data, error } = await fetcher(GET_HOME_SLIDER_COLLECTION)
+
+  // TODO: Check for errors
+  if (error) {
+    return {
+      props: {
+        error,
+        data,
+      },
+    }
+  }
+
+  const { sliderImagesCollection } = data.homeSliderCollection.items[0]
+
+  return {
+    props: {
+      images: sliderImagesCollection.items,
+      error,
+    },
+  }
+}
+
+export const Home = ({
+  error,
+  images,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const looped = images.length > 1
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [isReady, setIsReady] = useState(false)
+
+  const sliderIsReady = () => {
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    setIsReady(true)
+  }, [isReady])
+
+  if (isLoading && !isReady)
+    return (
+      <Image
+        src={images[0].url}
+        width={images[0].width}
+        height={images[0].height}
+        alt={images[0].title}
+        layout="raw"
+        priority
+      />
+    )
+
+  return (
+    <>
+      <HeroSlider looped={looped} sliderIsReady={sliderIsReady}>
+        {images.map((image: SliderImage, index: number) => (
+          <Image
+            key={index}
+            src={image.url}
+            width={image.width}
+            height={image.height}
+            alt={image.title}
+            layout="raw"
+            priority
+          />
+        ))}
+      </HeroSlider>
+      <div
+        style={{ backgroundColor: "red", width: "100%", height: "100px" }}
+      ></div>
+    </>
+  )
 }
 
 export default Home
