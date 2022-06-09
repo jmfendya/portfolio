db58eb62444f161bef9f63ee554d7f70a5a79be6
philipalexander <philip@dennisdevelopment.com>
2022-06-09 13:41:03 -0400

    feat: :sparkles: add site navigation

---
+++ content/navigation.ts
@@ -0,0 +1,6 @@
+export const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Experience", href: "/experience" },
+  { name: "Education", href: "/education" },
+  { name: "Contact", href: "/contact" },
+]
