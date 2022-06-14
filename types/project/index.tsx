d236fdb184e88659e051648e0a974cf259c1727d
philipalexander <philip@dennisdevelopment.com>
2022-06-13 21:57:07 -0400

    feat: :sparkles: add Project type

--- types/project/index.tsx
+++ types/project/index.tsx
@@ -1,3 +1,22 @@
+export type Project = {
+  sys: { id: string }
+  title: string
+  slug: string
+  category: string
+  employer: string
+  client: string
+  heroImage: {
+    sys: { id: string }
+    title: string
+    description: string
+    fileName: string
+    url: string
+    width: number
+    height: number
+  }
+  body: any
+}
+
 export type ProjectCardType = {
   sys: { id: string }
   title: string
