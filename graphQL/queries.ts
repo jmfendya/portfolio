80008038385d1a3d9e7a6019aa2821338a6ef149
philipalexander <philip@dennisdevelopment.com>
2022-06-13 22:06:00 -0400

    feat: :sparkles: add GET_PRODUCT query

--- graphQL/queries.ts
+++ graphQL/queries.ts
@@ -1,3 +1,80 @@
+// GET PROJECT
+export const GET_PROJECT = `#graphql
+  query getProject($slug: String!) {
+    projectCollection(where: { slug: $slug }, limit: 1) {
+      items {
+        title
+        slug
+        category
+        employer
+        client
+        heroImage {
+          sys {
+            id
+          }
+          title
+          description
+          fileName
+          url
+          width
+          height
+        }
+        body {
+          json
+          links {
+            entries {
+              inline {
+                sys {
+                  id
+                }
+              }
+              hyperlink {
+                sys {
+                  id
+                }
+              }
+              block {
+                sys {
+                  id
+                }
+              }
+            }
+            assets {
+              hyperlink {
+                sys {
+                  id
+                }
+              }
+              block {
+                sys {
+                  id
+                }
+                title
+                description
+                fileName
+                url
+                width
+                height
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+// GET PROJECT SLUGS
+export const GET_PROJECT_SLUGS = `#graphql
+  query {
+    projectCollection {
+      items {
+        slug
+      }
+    }
+  }
+`
+
 // GET HOME PAGE
 export const GET_HOME_PAGE = `#graphql
   query {
