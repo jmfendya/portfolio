245171be63d59a5f75beb533a94476ab6604da89
philipalexander <philip@dennisdevelopment.com>
2022-06-21 15:46:15 -0400

    feat: :sparkles: add theory and process navigation paths

--- content/navigation.ts
+++ content/navigation.ts
@@ -1,5 +1,7 @@
 export const navigation = [
   { name: "Home", href: "/" },
+  { name: "Theory", href: "/theory" },
+  { name: "Process", href: "/process" },
   { name: "Experience", href: "/experience" },
   { name: "Education", href: "/education" },
   { name: "Contact", href: "/contact" },
